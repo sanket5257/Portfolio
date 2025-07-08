ec271d7b5c06ce648c96d0c1313069e98d3b41d5
sanket <chougulesanket30@gmail.com>
2025-07-08 16:52:43 +0530

    feat: Enhance ProjectDetails layout with improved responsiveness and add arrow up icon

--- src/app/projects/[slug]/page.jsx
+++ src/app/projects/[slug]/page.jsx
@@ -31,55 +31,66 @@ export default function ProjectDetails({ params }) {
           />
         </div>
       </div>
-      <div id="section2" className="min-h-screen w-full">
-        <div className=" relative w-full">
-          <img
-            className="h-full w-full filter grayscale object-cover"
-            src={project.image}
-            alt=""
-          />
-          <img
-            className="h-[40vw] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 object-cover"
-            src="/project1hero.png"
-            alt=""
-          />
-          <div className="h-[25vh] w-[70vw] flex justify-evenly items-center bg-[#1D1D1D] text-[#D9D9D9] font-title absolute -bottom-20 left-1/2 transform -translate-x-1/2">
-            <div className="text-center">
-              <h3>Client name</h3>
-              <h3>Self</h3>
-            </div>
-            <div className="text-center">
-              <h3>Industry name</h3>
-              <h3>Self</h3>
-            </div>
-            <div className="text-center">
-              <h3>Services</h3>
-              <h3>Design & Dev</h3>
-            </div>
-            <div className="text-center">
-              <h3>Year</h3>
-              <h3>2025</h3>
-            </div>
+      {/* Section 2 */}
+      <div id="section2" className="min-h-screen w-full relative">
+        <img className="h-full w-full filter grayscale object-cover" src={project.image} alt="" />
+
+        <img
+          className="h-[60vw] sm:h-[50vw] md:h-[40vw] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 object-contain"
+          src="/project1hero.png"
+          alt=""
+        />
+
+        <div className="h-auto sm:h-[25vh] w-[90vw] sm:w-[70vw] flex flex-wrap justify-evenly items-center gap-4 p-4 bg-[#1D1D1D] text-[#D9D9D9] font-title absolute -bottom-20 left-1/2 transform -translate-x-1/2 text-sm sm:text-base">
+          <div className="text-center w-1/2 sm:w-auto">
+            <h3>Client name</h3>
+            <h3>Self</h3>
+          </div>
+          <div className="text-center w-1/2 sm:w-auto">
+            <h3>Industry name</h3>
+            <h3>Self</h3>
+          </div>
+          <div className="text-center w-1/2 sm:w-auto">
+            <h3>Services</h3>
+            <h3>Design & Dev</h3>
+          </div>
+          <div className="text-center w-1/2 sm:w-auto">
+            <h3>Year</h3>
+            <h3>2025</h3>
           </div>
         </div>
       </div>
+
+      {/* Section 3 */}
       <div id="section3" className="py-[20vw] w-full flex justify-center font-title items-center">
-        <div className="w-[70vw]  flex items-start gap-10 justify-between text-center">
-        <h1 className="w-[50vw] text-[1.5vw] font-extrabold">About</h1>
-        <p className="text-start text-[#666666] w-[50vw]">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates
-          necessitatibus voluptas atque nam aperiam nisi perferendis praesentium
-          autem tempora! Facilis, unde! Nihil in voluptatum, aspernatur rem
-          exercitationem tempora quaerat natus?
-        </p>
-        <p className="text-start text-[#666666] w-[50vw]">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam ipsa
-          ut voluptatum, molestiae, officia numquam odit maxime deleniti id
-          ducimus placeat sequi, ipsum eius consectetur praesentium ea aut esse!
-          Maiores?
-        </p>
+        <div className="w-[90vw] md:w-[70vw] flex flex-col md:flex-row items-start gap-10 text-center md:text-start">
+          <h1 className="md:w-[30%] text-[5vw] md:text-[1.5vw] font-extrabold">About</h1>
+          <div className="md:w-[70%] space-y-4 text-[#666666] text-sm md:text-base">
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates
+              necessitatibus voluptas atque nam aperiam nisi perferendis
+              praesentium autem tempora! Facilis, unde! Nihil in voluptatum,
+              aspernatur rem exercitationem tempora quaerat natus?
+            </p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam
+              ipsa ut voluptatum, molestiae, officia numquam odit maxime deleniti
+              id ducimus placeat sequi, ipsum eius consectetur praesentium ea aut
+              esse! Maiores?
+            </p>
+          </div>
         </div>
-        
+      </div>
+      {/* Call-to-action */}
+      <div className="relative px-4 sm:px-0">
+        <div className="h-[2px] w-full bg-[#1D1D1D] mb-10"></div>
+
+        <div className="h-[40vw] sm:h-[20vw] w-[40vw] sm:w-[20vw] rounded-full bg-[#1D1D1D] flex justify-center items-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <div className="flex flex-col sm:flex-row justify-center items-center gap-2 text-center">
+            <img className="size-8 sm:size-10" src="/arrowup.png" alt="" />
+            <h3 className="text-[#D9D9D9] text-xs sm:text-base">SEE LIVE SITE</h3>
+          </div>
+        </div>
       </div>
     </div>
   );
